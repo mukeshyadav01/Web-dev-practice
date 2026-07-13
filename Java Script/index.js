7d218855be35b390aa3dafe48960eab68a130d38
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-13 22:01:12 +0530

    fundamentals of Java Script

---
+++ Java Script/index.js
@@ -0,0 +1,77 @@
+// variable kaise banate hai 
+// let name = "Mukesh";
+// let age = 23;
+// age = 33;
+// console.log(name , age);
+// const account= 2345;
+// // account = 2343;
+// console.log(account);
+// old method 
+// var a = 10;
+// a = 39;
+// console.log(a)
+
+// DATA TYPE 
+// 1. PRIMITIVE DATA TYPE 
+// Number , string , boolean , Undefined , null , bigint , symbol 
+
+// number 
+// let a = 10;
+// let b = 2.34;
+// console.log(a,b)
+
+// // String
+// let c = "Mukesh will become an IAS officer";
+// let d = "Anjali";
+// console.log(c,d);
+
+// // boolean
+// let login = true;
+// let f = false;
+// console.log(login , f);
+
+
+// // Undefined 
+// let user;
+// console.log(user);
+
+// bigint
+// let num = 353465333453n;
+// console.log(num);
+
+// null
+// let weather = currnt_weather("Silvassa");
+// 23
+// null
+// undefined
+
+//  Symbol
+
+// const id1 = Symbol("id");
+// console.log(id1);
+
+
+// NON-PRIMITVE DATA TYPE
+// ARRAY , OBJECT , FUNCTION
+
+// // ARRAY
+// let arr = [20,88, "Mukesh" , true];
+// console.log(arr);
+// Object
+// let user = {
+//     name:"Mukesh",
+//     account: 388,
+//     age:9,
+//     category:"OBC"
+// }
+
+// Function
+function add(){
+    console.log("hello");
+}
+add();
+
+let s = function add(){
+    console.log("hello Mukesh");
+}
+console.log(s);
---
+++ Java Script/index1.js
@@ -0,0 +1,8 @@
+// Operator
+// Arithmetic
+console.log(3+7);
+console.log(7*8);
+console.log(7-3);
+console.log(5%2);
+console.log(6/2);
+console.log(5**3)
