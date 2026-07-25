2fbd56a28db538525d0f93bd133d6101be0f6547
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-07-25 22:34:47 +0530

    Updating the Portfolio

--- Java Script/index1.js
+++ Java Script/index1.js
@@ -1,8 +1,109 @@
 // Operator
 // Arithmetic
-console.log(3+7);
-console.log(7*8);
-console.log(7-3);
-console.log(5%2);
-console.log(6/2);
-console.log(5**3)
+// console.log(3+7);
+// console.log(7*8);
+// console.log(7-3);
+// console.log(5%2);
+// console.log(6/2);
+// console.log(5**3)
+
+// // Assignment Operator
+// let x = 39;
+// let y = 87;
+// // x = x + y;
+// x-= y;
+// console.log(x);
+
+// COMPARISON OPERATOR
+// let x = 20;
+// let y = 10;
+// console.log(x>y)
+// console.log(x>=y)
+// console.log(x<y)
+// console.log(x<=y)
+// console.log(x==y)
+
+// String -> Number;
+// let z = "88" ;
+// let a = Number(z);
+// console.log(a)
+// console.log(typeof a)
+
+// let a = "234la";
+// let b = Number(a);
+// console.log(b);
+// console.log(typeof a)
+
+// Number -> String
+// let a = 29;
+// let b = String(a);
+// console.log(b);
+// console.log(typeof b)
+
+// console.log(Number(true));
+// console.log(Number(false));
+
+
+// COMPUTER SCIENCE CHALLENGE
+// let a = 0.2;
+// let b = 0.1;
+// let c = a + b ;
+// console.log(c==0.3)
+
+// 1. Null is loosely equal to undefined only
+// console.log(null==undefined);
+// console.log(null===undefined);
+// console.log(null== 0);
+// console.log(null== false);
+// console.log(null== true);
+
+//  > , < , >= , <= (null -> Number)
+// console.log(null>= 0);
+// console.log(null<= 0);
+// console.log(null> 0);
+// console.log(null< 0);
+// console.log(null>= undefined);
+
+// for loop 
+// for(let i = 0; i<9;i++)
+// {
+//     console.log("Mukesh");
+// }
+
+// while loop 
+// let i = 0 ;
+// while(i<9)
+// {
+//     console.log("mukesh");
+//     i++;
+// }
+
+// do while
+// let i= 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<4)
+
+// if else condition
+// let age = 6;
+// if(age>18)
+// {
+//     console.log("Eligeble of vote")
+// }
+// else{
+//     console.log("not ")
+// }
+
+// LOGICAL OPERATOR
+// && , || 
+// console.log(true && true);
+// console.log(true && false);
+// console.log(true || true);
+// console.log(true || false);
+// console.log(false && false);
+
+let a = "mukesh";
+let b = "Anuj";
+let c = a&&b;
+console.log(c)
