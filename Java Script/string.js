5c9af28b7228b97c5042e972edee236ab04bd29c
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-08-22 20:41:12 +0530

    String in Java Script

---
+++ Java Script/string.js
@@ -0,0 +1,47 @@
+// String
+// const str1 = "Mukesh ";
+// const str2 = 'Mukesh yadav';
+// const day = 3 
+// const str3 = `IAS  Mukesh Yadav banane me bus ${day} years `;
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
+
+// const str= `hello mukesh yadav`;
+// console.log(str.length)
+// console.log(str[8])
+
+// // Upper case 
+// console.log(str.toUpperCase())
+
+// const str = `hello mukesh kaise ho `;
+// console.log(str.indexOf('ks'));
+// console.log(str.includes('sh'))
+
+// // Slice
+// // console.log(str.slice(2,7));
+// console.log(str.slice(-5))
+
+// const str= `hello Mukesh `;
+// console.log(str.substring(3,6))
+
+// const a = 'mukesh'
+// const b = 'yadav'
+// const c = a+" "+b
+// console.log(c)
+
+// const str = `hello Mukesh Yadav`;
+// console.log (str.replaceAll("esh",'iam'));
+// console.log(str)
+
+// const user = "     Mukesh ";
+// console.log(user.trim())
+// const names = "Mukesh , Rohit , Ramesh , Suresh";
+// console.log(names.split(","))
+
+// const now = new Date();
+// console.log(now);
+// console.log(now.toString());
+// console.log(now.toLocaleString());
+
+
