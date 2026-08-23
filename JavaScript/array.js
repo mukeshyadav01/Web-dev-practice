08776088132824a2e80f5ec330fb28164ada5fb7
Mukesh Yadav <mukesh.ydv.012003@gmail.com>
2026-08-23 21:55:17 +0530

    Learnig Array in JavaScript

---
+++ JavaScript/array.js
@@ -0,0 +1,47 @@
+// Array;
+let marks = [34,33,54,23];
+// console.log(marks);
+// console.log(marks.length);
+
+// Add elements to End
+// marks.push("mukesh");
+// marks.push(99);
+// console.log(marks)
+
+// Delete Elements to end 
+// marks.pop()
+// console.log(marks)
+
+//  let arr = [10,28,38,73,33];
+// // for(let i=0;i<arr.length;i++)
+// // {
+// //     console.log(arr[i]);
+// // }
+// for(let num of arr){
+//     console.log(num);
+// }
+// const arr = [3,53,64,23,66,32];
+// let arr1 = arr;
+// arr1.push(3);
+// console.log(arr1);
+
+// Slicing
+// const arr = [3,53,64,23,66,32];
+// const arr1 = arr.slice(2,4)
+// console.log(arr1)
+
+// Spread Operator
+// const arr = [3,53,64,23,66,32];
+// const arr2 = ["Mukesh",33,true];
+// const arr4 = ["Yadav",23,false]
+// arr.push(arr2);
+// const arr3 = arr.concat(arr2)
+// const arr3= [arr,arr2,arr4]
+// using spread
+// const arr3= [...arr,...arr2,...arr4]
+// console.log(arr3)
+
+// Converting array to string
+const names = ["Alice","Bob","Charlie"];
+// console.log(names.toString())
+console.log(names.join("-"))
